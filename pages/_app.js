64cabae2b4f1f8c510e63a409341c8d4b457fcd4
James Grant <jamespgrant3@gmail.com>
2023-02-15 17:40:19 -0500

    added s3 hack

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,15 @@
 import '../styles/global.css'
+import {useRouter} from 'next/router'
 
 export default function App({ Component, pageProps }) {
+  // hack to get around s3 redirect rule, to avoid access denied errors on refresh
+  const router = useRouter();
+
+  const path = (/#!(\/.*)$/.exec(router.asPath) || [])[1];
+
+  if (path) {
+    router.replace(path);
+  }
+
   return <Component {...pageProps} />
 }
